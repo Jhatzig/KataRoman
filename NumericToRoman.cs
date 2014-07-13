06c7a7e2a9796874211d74c67b18afecc74326bc
Jhatzig <j.hatzig@gmail.com>
2014-07-13 22:27:41 +0200

    Dojo n° 3 - KataRoman
    Dojo n ° 3
    Méthodologie TDD
    Premiere version

---
+++ NumericToRoman.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KataRoman
+{
+    public class KataRoman
+    {
+        public Dictionary<string, int> dicto { get; private set; }
+
+        public KataRoman()
+        {
+            dicto = new Dictionary<string, int>();
+            dicto.Add("M", 1000);
+            dicto.Add("CM", 900);
+            dicto.Add("D", 500);
+            dicto.Add("CD", 400);
+            dicto.Add("C", 100);
+            dicto.Add("XC", 90);
+            dicto.Add("L", 50);
+            dicto.Add("XL", 40);
+            dicto.Add("X", 10);
+            dicto.Add("IX", 9);
+            dicto.Add("V", 5);
+            dicto.Add("IV", 4);
+            dicto.Add("I", 1);
+        }
+
+        /// <summary>
+        /// Methode d'entrée qui permet de convertir en Roman ou Numeric.
+        /// </summary>
+        /// <param name="input">String Entrée.</param>
+        /// <returns>String Resultat.</returns>
+        public string Transform(string input)
+        {
+            int result;
+            return int.TryParse(input, out result) ? NumToRoman(result) : RomanToNum(input);
+        }
+
+
+        /// <summary>
+        /// Permet de convertir des caractères Romain en Numérique
+        /// </summary>
+        /// <param name="input">String Input.</param>
+        /// <returns>String Resultat.</returns>
+        public string RomanToNum(string input)
+        {
+            int value = 0;
+            CheckSpecialRomanCharacters(ref input, ref value, "CM");
+            CheckSpecialRomanCharacters(ref input, ref value, "CD");
+            CheckSpecialRomanCharacters(ref input, ref value, "XC");
+            CheckSpecialRomanCharacters(ref input, ref value, "XL");
+            CheckSpecialRomanCharacters(ref input, ref value, "IX");
+            CheckSpecialRomanCharacters(ref input, ref value, "IV");
+            foreach (var item in dicto)
+            {
+                if (string.Equals(input, item.Key))
+                {
+                    value = item.Value;
+                }
+                else
+                {
+                    char[] letters = input.ToCharArray();
+                    foreach (var letter in letters)
+                    {
+                        if (string.Equals(letter.ToString(), item.Key))
+                        {
+                            value += item.Value;
+                        }
+                    }
+                }
+            }
+            return value == 0 ? string.Empty : value.ToString();
+        }
+
+        /// <summary>
+        /// Controlle les caractères spéciaux "romains"
+        /// </summary>
+        /// <param name="input"></param>
+        /// <param name="value"></param>
+        /// <param name="carac"></param>
+        private void CheckSpecialRomanCharacters(ref string input, ref int value, string carac)
+        {
+            if (input.Contains(carac))
+            {
+                value += dicto.Where(x => x.Key == carac).First().Value;
+                input = input.Replace(carac, " ");
+            }
+        }
+
+        /// <summary>
+        /// Permet de transformer un numérique en "Roman"
+        /// </summary>
+        /// <param name="nb"></param>
+        /// <returns></returns>
+        public string NumToRoman(int nb)
+        {
+            string result = string.Empty;
+            if (nb == 0)
+            {
+                result = string.Empty;
+            }
+            else
+            {
+                while (nb > 0)
+                {
+                    foreach (var item in dicto)
+                    {
+                        if (nb != 0)
+                        {
+                            if (item.Value == nb)
+                            {
+                                nb = nb - item.Value;
+                                result += item.Key;
+                            }
+                            if (nb - item.Value > 0)
+                            {
+                                while ((nb - item.Value) >= 0)
+                                {
+                                    nb = nb - item.Value;
+                                    result += item.Key;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ NumericToRomanTest.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace KataRoman
+{
+    [TestClass]
+    public class NumericToRomanTest
+    {
+
+        KataRoman kr;
+
+        /// <summary>
+        /// Initialise les tests
+        /// </summary>
+        [TestInitialize]
+        public void Init()
+        {
+            kr = new KataRoman();
+        }
+        
+        /// <summary>
+        /// Permet de tester si ca retourne vide quand on envoie 0.
+        /// </summary>
+        [TestMethod]
+        public void ReturnEmptyIfNumericIsZeroTest()
+        {
+            var output = kr.Transform("0");
+            Assert.AreEqual(string.Empty, output); 
+        }
+
+        /// <summary>
+        /// Permet de tester si on renvoi vide quand on envoie null.
+        /// </summary>
+        [TestMethod]
+        public void ReturnEmptyIfRomanIsNullTest()
+        {
+            var output = kr.Transform("");
+            Assert.AreEqual(string.Empty, output);
+        }
+
+        /// <summary>
+        /// Permet de tester la conversion des chiffres numériques vers les chiffres romains.
+        /// </summary>
+        [TestMethod]
+        public void NumToRomanTest()
+        {
+            var output = kr.NumToRoman(1);
+            Assert.AreEqual("I", output);
+            output = kr.NumToRoman(5);
+            Assert.AreEqual("V", output);
+            output = kr.NumToRoman(10);
+            Assert.AreEqual("X", output);
+            output = kr.NumToRoman(4);
+            Assert.AreEqual("IV", output);
+            output = kr.NumToRoman(199);
+            Assert.AreEqual("CXCIX", output);
+        }
+
+        /// <summary>
+        /// Permet de tester la convertion des chiffres romains en numérique.
+        /// </summary>
+        [TestMethod]
+        public void RomanToNumTest()
+        {
+            var output = kr.RomanToNum("I");
+            Assert.AreEqual("1", output);
+            output = kr.RomanToNum("VIII");
+            Assert.AreEqual("8", output);
+            output = kr.RomanToNum("III");
+            Assert.AreEqual("3", output);
+            output = kr.RomanToNum("XII");
+            Assert.AreEqual("12", output);
+            output = kr.RomanToNum("IV");
+            Assert.AreEqual("4", output);
+            output = kr.RomanToNum("CXCIX");
+            Assert.AreEqual("199", output);
+        }
+
+
+        [TestMethod]
+        public void NumericOrRomanTest()
+        {
+
+            var output = kr.Transform("12");
+            Assert.AreEqual("XII", output);
+            output = kr.Transform("8");
+            Assert.AreEqual("VIII", output);
+            output = kr.Transform("80");
+            Assert.AreEqual("LXXX", output);
+            output = kr.Transform("1");
+            Assert.AreEqual("I", output);
+
+            output = kr.Transform("XII");
+            Assert.AreEqual("12", output);
+            output = kr.Transform("VIII");
+            Assert.AreEqual("8", output);
+            output = kr.Transform("LXXX");
+            Assert.AreEqual("80", output);
+            output = kr.Transform("I");
+            Assert.AreEqual("1", output);
+        }
+        
+    }
+}
